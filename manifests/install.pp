c982fc66078b81b95a858bb31a8eeef9816d6404
Sam Beale <sam@easy-forex.com>
2016-07-13 02:21:52 +0300

    Test compatibility without unless

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,10 +3,8 @@
 # This class installs the application.
 #
 class redis::install {
-  unless defined(Package['$::redis::package_name']) {
-    ensure_resource('package', $::redis::package_name, {
+	ensure_resource('package', $::redis::package_name, {
       'ensure' => $::redis::package_ensure
     })
-  }
 }
 
